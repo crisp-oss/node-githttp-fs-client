3c926c2728b0e34968799cbf6cf9aa1f0c0c1a11
Valerian Saliou <valerian@valeriansaliou.name>
2026-07-03 14:00:18 +0100

    Update example

--- examples/simple.ts
+++ examples/simple.ts
@@ -12,20 +12,46 @@ const client = new GitHTTPFSClient({
   apiKey: "MySecretAPIKey"
 });
 
-const COLLECTION_ID = "my-collection";
-const TENANT_ID = "4ffb599d-4115-42b4-a568-5537ce97f56c";
-const FILE_PATH = "guides/index.md";
+const COLLECTION_ID = "example-simple";
+const TENANT_ID = "t_1";
+const FILE_PATH = "hello-world/index.md";
+
+const AUTHOR = {
+  name: "Valerian Saliou",
+  email: "valerian@valeriansaliou.name"
+};
 
 try {
+  // Write file
+  const writeResult = await client.writeFile(COLLECTION_ID, TENANT_ID, FILE_PATH, {
+    content: "This is an example file.",
+    author: AUTHOR,
+    message: "chore: add example file"
+  });
+
+  console.log("Wrote file: " + FILE_PATH, writeResult);
+
   // List files for tenant
   const files = await client.listFiles(COLLECTION_ID, TENANT_ID);
 
   console.log("Listed files:", files);
 
-  // Get a specific file
+  // List commits for tenant
+  const commits = await client.listCommits(COLLECTION_ID, TENANT_ID, 1, 3);
+
+  console.log("Listed latest commits:", commits);
+
+  // Get file
   const content = await client.getFileContent(COLLECTION_ID, TENANT_ID, FILE_PATH);
 
   console.log("Got content:", content);
+
+  // Delete file
+  const deleteResult = await client.deleteFile(COLLECTION_ID, TENANT_ID, FILE_PATH, {
+    author: AUTHOR
+  });
+
+  console.log("Deleted file: " + FILE_PATH, deleteResult);
 } catch (error) {
   console.error("Error", error);
 }
